c7e1f6856a031a903605a359ef53e427ec4e274f
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-20 20:06:03 +0300

    start refactoring whith decorators

---
+++ src/controllers/decorators/routes.ts
@@ -0,0 +1,8 @@
+import 'reflect-metadata';
+
+export function get(path: string) {
+
+  return function(target: any, key: string) {
+    Reflect.defineMetadata('path', path, target, key);
+  };
+} 
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import { router as controllerRouter } from './controllers/decorators/controller';
+import './controllers/LoginController';
 
 
 const app = express();
@@ -9,7 +11,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['strings'] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
-})
+});
